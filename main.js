4a6057f03152945689b5a203116555fbd762a766
aaacms <138058617+aaacms@users.noreply.github.com>
2023-07-03 17:07:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio); //verifica se o elemento é nulo 
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`//template string
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === "Enter") {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
+
+//enquanto
+/*let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
+    contador = contador + 1;
+} */
